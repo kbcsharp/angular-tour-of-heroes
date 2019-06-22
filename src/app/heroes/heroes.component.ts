642024996fcca42898ecf39c6ab7ec14a10a0e78
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-06-21 23:25:23 -0400

    the application shell complete 5 up to update heroes in 6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
 import { HeroService } from "../hero.service";
 
 @Component({
@@ -9,12 +8,12 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.scss"]
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "Windstorm"
+  // };
 
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
   heroes: Hero[];
 
@@ -24,9 +23,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
